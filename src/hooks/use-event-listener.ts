1c25251b6023662b1151ab85130ae9f82dda4cf7
Adryel Bezerra <adryelrbrz+github@gmail.com>
2025-11-19 23:01:41 +0000

    feat(hooks): add useEventListener hook

---
+++ src/hooks/use-event-listener.ts
@@ -0,0 +1,35 @@
+import { useEffect, useRef } from 'react';
+
+type EventMap = WindowEventMap & DocumentEventMap;
+
+type UseEventListener = <K extends keyof EventMap>(
+	target: EventTarget,
+	eventName: K,
+	handler: (event: EventMap[K]) => void,
+	options?: boolean | AddEventListenerOptions,
+) => void;
+
+export const useEventListener: UseEventListener = (
+	target,
+	eventName,
+	handler,
+	options,
+) => {
+	const savedHandler = useRef(handler);
+
+	useEffect(() => {
+		savedHandler.current = handler;
+	}, [handler]);
+
+	useEffect(() => {
+		const callback = (event: Event) => {
+			savedHandler.current(event as EventMap[typeof eventName]);
+		};
+
+		target.addEventListener(eventName, callback, options);
+
+		return () => {
+			target.removeEventListener(eventName, callback, options);
+		};
+	}, [target, eventName, options]);
+};
