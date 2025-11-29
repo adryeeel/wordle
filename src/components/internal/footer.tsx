d7c2afb61abb191db16d317d4ba981487a21de38
Adryel Bezerra <adryelrbrz+github@gmail.com>
2025-11-29 23:56:54 +0000

    feat(components): add footer component

---
+++ src/components/internal/footer.tsx
@@ -0,0 +1,8 @@
+export function Footer() {
+	return (
+		<footer className='p-1 text-center text-xs text-zinc-500 sm:p-2 sm:text-sm'>
+			<q>I am the Way and the Truth and the Life.</q>
+			<i> &mdash; John 14:6</i>
+		</footer>
+	);
+}
