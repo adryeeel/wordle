b1b91ca393273b859e9118b08458396788e74168
Adryel Bezerra <adryelrbrz+github@gmail.com>
2025-11-29 23:55:41 +0000

    feat(components): add barrel file for internal components

---
+++ src/components/internal/index.ts
@@ -0,0 +1,7 @@
+export { Logo } from './logo';
+export { Footer } from './footer';
+export { Github } from './github';
+export { Header } from './header';
+export { Flipper } from './flipper';
+export { ThemeToggle } from './theme';
+export { Sequential } from './sequential';
