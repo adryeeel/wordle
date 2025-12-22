59c705e3622ef7606650975b33976894894743f5
Adryel Bezerra <adryelrbrz+github@gmail.com>
2025-12-22 22:20:33 +0000

    feat(grid): add barrel file

---
+++ src/features/grid/index.ts
@@ -0,0 +1,7 @@
+export type { RowsSlice } from './types/rows';
+export type { GridSlice, Position } from './types/grid';
+
+export { createRowSlice } from './store/slice-row';
+export { createGridSlice } from './store/slice-grid';
+
+export { Grid } from './components/grid';
